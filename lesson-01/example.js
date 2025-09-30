533b20d0d9e2d761dcdbcfe668b9a598c9f40029
Andrey Drobysh <adrobysh@alfabank.ru>
2025-09-30 09:04:40 +0400

    first commit

---
+++ lesson-01/example.js
@@ -0,0 +1,3 @@
+let name = "Иван";
+const age = 25;
+console.log(name, age);
---
+++ lesson-01/practice.js
@@ -0,0 +1,20 @@
+const prompt = require("prompt-sync")({sigint: true});
+
+// TODO: создай переменные username, age и выведи их в консоль
+let username = 'Olesya';
+let age = prompt("How old are you? ");;
+let password;
+const p = 3.14;
+if (false) {
+    console.log(`Welcome to the ${username}!`);
+}
+if (age > 40){
+    console.log('вам больше 40')
+} else if (age > 10){
+    console.log('вам больше 10')
+} else {
+    console.log('something went wrong!');
+}
+// sigint=true, чтобы Ctrl+C работал
+
+
