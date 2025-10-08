3c1b75ea741201b0e3b805a61283e13d8becfa1a
lesya <niklesenok@gmail.com>
2025-10-08 18:15:34 +0400

    game

---
+++ lesson 3/hero&boss.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")({sigint: true});
+
+let myNumber1 = Number(prompt("Enter your first number"));
+let myNumber2 = Number(prompt("Enter your second number"));
+let compNumber1 = Math.floor(Math.random() * 6);
+let compNumber2 = Math.floor(Math.random() * 6);
+console.log("comp choosing: " + compNumber1);
+console.log("comp choosing: " + compNumber1);
